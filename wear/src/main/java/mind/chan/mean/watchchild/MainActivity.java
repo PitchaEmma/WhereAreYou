8c94385928b0b72f9b9b2ba091fc57788e34a049
MasterUNG <phrombutr@gmail.com>
2017-08-21 12:41:20 +0700

    Initial commit

---
+++ wear/src/main/java/mind/chan/mean/watchchild/MainActivity.java
@@ -0,0 +1,33 @@
+package mind.chan.mean.watchchild;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }   // Main Method
+
+
+}   // Main Class
